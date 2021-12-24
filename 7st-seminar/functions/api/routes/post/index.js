9f441dcdf135260bde06b13384602449b0b762a2
eunji8784 <eunjikim8784@naver.com>
2021-12-24 20:17:45 +0900

    :sparkles: [FEAT]: Firebase Authentication을 이용한 로그인 구현

--- 7st-seminar/functions/api/routes/post/index.js
+++ 7st-seminar/functions/api/routes/post/index.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
+const { checkUser } = require("../../../middlewares/auth");
 const router = express.Router();
 
-router.get('/list', require('./postListGET'));
-router.post('/', require('./postPOST'));
+router.get("/list", require("./postListGET"));
+router.post("/", require("./postPOST"));
 
-router.get('/:postId', require('./postGET'));
-router.put('/:postId', require('./postPUT'));
-router.delete('/:postId', require('./postDELETE'));
+router.get("/:postId", checkUser, require("./postGET"));
+router.put("/:postId", require("./postPUT"));
+router.delete("/:postId", require("./postDELETE"));
 
 module.exports = router;
