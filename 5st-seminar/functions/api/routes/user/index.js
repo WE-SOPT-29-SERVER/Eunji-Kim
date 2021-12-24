fda9888f8f8935aa101dfd63185962cc1326a1ed
eunji8784 <eunjikim8784@naver.com>
2021-12-24 17:01:47 +0900

    :white_check_mark: [CHORE]: 라우터 정리

--- 5st-seminar/functions/api/routes/user/index.js
+++ 5st-seminar/functions/api/routes/user/index.js
@@ -1,11 +1,11 @@
-const express = require("express")
+const express = require('express');
+const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
-// '/user/signup'으로 오는 요청을 userSignupPOST 파일에서 처리
-router.post("/signup", require("./userSignupPOST"));
-router.post("/login", require("./userLoginPOST"));
-router.get("/profile/:id", require("./userProfileGET"));
-router.delete("/profile/:id", require("./userProfileDELETE"));
-router.put("/profile/:id", require("./userProfilePUT"));
+router.get('/list', require('./userListGET'));
+
+router.get('/:userId', require('./userGET'));
+router.put('/:userId', checkUser, require('./userPUT'));
+router.delete('/:userId', require('./userDELETE'));
 
 module.exports = router;
